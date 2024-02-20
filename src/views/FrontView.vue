fbfe5d433d4fb583ec2685fb9e51c00ed75af202
Macy Huang <mjmj3794@gmail.com>
2024-02-20 19:55:06 +0800

    git add .

---
+++ src/views/FrontView.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>這是前台</h2>
+    <nav>
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/about">About</RouterLink> |
+        <RouterLink to="/productslist">產品清單</RouterLink> |
+        <RouterLink to="/cart">購物車清單</RouterLink> |
+        <RouterLink to="/login">登入</RouterLink> |
+        <RouterLink to="/admin/products">後台 - 產品列表</RouterLink>
+    </nav>
+    <RouterView></RouterView>
+</template>
